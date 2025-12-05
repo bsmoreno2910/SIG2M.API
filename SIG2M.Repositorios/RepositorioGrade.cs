49f5a923e6a83ed09ddd4a029b8d54f361731755
BRUNO SILVA MORENO <bruno.moreno@vtclog.com.br>
2025-12-05 16:32:02 -0300

    grade

---
+++ SIG2M.Repositorios/RepositorioGrade.cs
@@ -0,0 +1,42 @@
+﻿using SIG2M.Dominio.Entidades;
+using SIG2M.Dominio.Interfaces.Repositorios.Grade;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIG2M.Repositorios
+{
+    public class RepositorioGrade : IRepositorioGrade
+    {
+        public Task<bool> AtualizarStatusAsync(Grade grade, IDbConnection conn, IDbTransaction transaction = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        //public Task<bool> AtualizarStatusAsync(Grade grade, IDbConnection conn, IDbTransaction transaction = null)
+        //{
+        //    string commandText = @" UPDATE grade
+        //                            SET status='A'
+        //                            where sigla = @sigla
+        //                              AND cod_material = @cod_material
+        //                              AND qtde = @qtde
+        //                              AND funci = @funci
+        //                              AND obs = @obs
+        //                              AND status = @status
+        //                              and tipo = @tipo
+        //                              AND data = @data;";
+
+
+
+
+        //}
+
+        public Task<IEnumerable<Grade>> PedidosPendentesAsync(IDbConnection conn, IDbTransaction transaction = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
